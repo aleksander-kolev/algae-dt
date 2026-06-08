d8632c08e438339bc48e4de731393f537d506f5f
Kremena <kremena.m.krumova@gmail.com>
2026-06-08 10:54:52 +0200

    Cleaned up test prints from bloom_predictor

--- ros2_ws/src/algae_dt/algae_dt/lib/bloom_predictor.py
+++ ros2_ws/src/algae_dt/algae_dt/lib/bloom_predictor.py
@@ -30,9 +30,6 @@ params = {
 responses = openmeteo.weather_api(url, params = params)
 
 response = responses[0]
-print(f"Coordinates: {response.Latitude()}°N {response.Longitude()}°E")
-print(f"Elevation: {response.Elevation()} m asl")
-print(f"Timezone difference to GMT+0: {response.UtcOffsetSeconds()}s")
 
 hourly = response.Hourly()
 sea_surface_temperature = hourly.Variables(0).ValuesAsNumpy()
